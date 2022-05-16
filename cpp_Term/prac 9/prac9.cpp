39fa09107e6dd767407f1596457dfd56c336554f
unknown <timofeev2.as@edu.spbstu.ru>
2022-05-16 18:10:08 +0300

    improve 9practice

--- cpp_Term/prac 9/prac9.cpp
+++ cpp_Term/prac 9/prac9.cpp
@@ -15,11 +15,12 @@
 
 using namespace std;
 
-class Matrix{
+
+class Matrix {
 public:
 	vector <vector <int>> matrix;
 	int n, m;
-	Matrix(vector<vector<int>> vec, int n,int m) {
+	Matrix(vector<vector<int>> vec, int n, int m) {
 		this->matrix.resize(n, std::vector<int>(m, 0));
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
@@ -29,7 +30,16 @@ public:
 		this->n = n;
 		this->m = m;
 	}
-
+	Matrix operator+(Matrix& A) {
+		vector<vector<int>>vec(A.n, vector<int>(A.m, 0));
+		for (int i = 0; i < A.n; i++) {
+			for (int j = 0; j < A.m; j++) {
+				vec[i][j] = A.matrix[i][j] + this->matrix[i][j];
+			}
+		}
+		Matrix X(vec, A.n, A.m);
+		return X;
+	}
 	void transpose_Matrix() {
 		vector<vector<int>>temp;
 		temp = matrix;
@@ -40,7 +50,7 @@ public:
 		}
 	}
 	void set_Matrix_values(vector<vector<int>> vec) {
-		
+
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				matrix[i][j] = vec[i][j];
@@ -56,7 +66,7 @@ public:
 		}
 	}
 
-	void plus_Matrix(Matrix Y){
+	void plus_Matrix(Matrix Y) {
 		for (int i = 0; i < matrix.size(); i++) {
 			for (int j = 0; j < matrix[i].size(); j++) {
 				matrix[i][j] += Y.matrix[i][j];
@@ -66,18 +76,18 @@ public:
 
 
 };
-
 Matrix sum_of_two(Matrix A, Matrix B) {
 	vector<vector<int>>vec(A.n, vector<int>(A.m, 0));
 	for (int i = 0; i < A.n; i++) {
 		for (int j = 0; j < A.m; j++) {
 			vec[i][j] = A.matrix[i][j] + B.matrix[i][j];
 		}
+		Matrix C(vec, A.n, A.m);
+		return C;
 	}
-
-	Matrix C(vec, A.n, A.m);
-	return C;
 }
+
+
 
 
 int main() {
@@ -88,6 +98,7 @@ int main() {
 	*/
 	vector<vector<int>> vec1(3, vector <int>(3));
 	vector<vector<int>> vec2(3, vector <int>(3));
+	vector<vector<int>> vec3(3, vector <int>(3, 0));
 	for (int i = 0; i < vec1.size(); i++) {
 		for (int j = 0; j < vec1[i].size(); j++) {
 			cin >> vec1[i][j];
@@ -98,15 +109,18 @@ int main() {
 			cin >> vec2[i][j];
 		}
 	}
-	Matrix P1(vec1, 3, 3);	
+	Matrix P1(vec1, 3, 3);
 	Matrix P2(vec2, 3, 3);
-	//Matrix P3 = sum_of_two(P1, P2);
+
 	//P1.transpose_Matrix();
 	//P1.print_Matrix_values();
-
-	P1.plus_Matrix(P2);
+	//P2.print_Matrix_values();
+	//P1.plus_Matrix(P2);
+	Matrix P3(vec3, 3, 3);
+	P3 = P2 + P1;
 	P1.print_Matrix_values();
-
+	P2.print_Matrix_values();
+	P3.print_Matrix_values();
 
 	return 0;
 }
